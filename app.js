707cccab26f2d825f953484c89f2ff896b452bd7
petushka1 <nadya@devery.io>
2022-05-09 20:42:42 +0300

    added close icon and functionality

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const hamBurger = document.querySelector('.hambuger');
+
 function clicked () {
   let div = document.createElement('div');
   div.className = 'modal';
+  let img = document.createElement('img');
+  img.className = 'icon';
+  img.src = './images/close.png';
+  div.appendChild(img);
+
+  img.addEventListener('click', closed);
+  function closed() {
+    div.style.display = 'none';
+  }
+
   let ul = document.createElement('ul')
    let li = document.createElement('li')
    li.textContent = 'Portfolio';
@@ -22,4 +33,5 @@ function clicked () {
    const wrapper = document.querySelector('.wrapper');
    wrapper.appendChild(div);
   }
+
   hamBurger.addEventListener('click', clicked);
