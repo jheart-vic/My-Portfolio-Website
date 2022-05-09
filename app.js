cd1a7571b805aba6eb659e031eb8e2b76c682f6c
jheart-vic <adebowalevictorjheart.com>
2022-05-09 15:55:03 +0100

    Add the functionality to the menu button

---
+++ app.js
@@ -0,0 +1,25 @@
+const hamBurger = document.querySelector('.hambuger');
+function clicked () {
+  let div = document.createElement('div');
+  div.className = 'modal';
+  let ul = document.createElement('ul')
+   let li = document.createElement('li')
+   li.textContent = 'Portfolio';
+   li.style.listStyle = 'none'
+   ul.appendChild(li);
+   li = document.createElement('li');
+   li.textContent = 'About';
+   li.style.listStyle = 'none'
+   ul.appendChild(li);
+   li = document.createElement('li');
+   li.textContent = 'contact'
+   ul.appendChild(li);
+   li.className = 'nav-link';
+   li.style.listStyle = 'none'
+   ul.className = "nav-links";
+   ul.style.display = 'block';
+   div.appendChild(ul);
+   const wrapper = document.querySelector('.wrapper');
+   wrapper.appendChild(div);
+  }
+  hamBurger.addEventListener('click', clicked);
