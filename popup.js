bd9fface70985d9fc455b448d74b94363aa60913
jheart-vic <adebowalevictorjheart.com>
2022-05-11 16:36:54 +0100

    Add eslint to linter

--- popup.js
+++ popup.js
@@ -21,7 +21,7 @@ const popUps = [
   },
   {
     id: 'project-button3',
-    title: 'Data Dashboard Healthcare',
+    title: 'Dashboard Healthcare',
     image: './images/SnapshootPortfolio.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
     technologies: ['html', 'bootstrap', 'Ruby'],
@@ -50,7 +50,7 @@ const popUps = [
   },
   {
     id: 'project-button5',
-    title: 'Data Dashboard Healthcare',
+    title: 'Dashboard Healthcare',
     image: './images/SnapshootPortfolio.png',
     description:
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
@@ -83,7 +83,7 @@ function popUpDiv(id) {
   divTitle.classList.toggle('popup-title');
 
   divParagraph.innerText = `${popUps[id].description}`;
-  //divParagraph.className = 'popup-paragraph';
+  // divParagraph.className = 'popup-paragraph';
   divParagraph.classList.toggle('popup-paragraph');
   divLiveBtn.textContent = 'see live';
   divLiveBtn.className = 'live';
