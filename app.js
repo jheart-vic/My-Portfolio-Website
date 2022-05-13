6179bde5933353c660623752a1be6d0235eb2349
jheart-vic <adebowalevictorjheart.com>
2022-05-13 18:26:32 +0100

    Made changes

--- app.js
+++ app.js
@@ -99,8 +99,6 @@ form.addEventListener('change', () => {
   users.name = document.getElementById('name').value;
   users.text = document.getElementById('text-area').value;
 
-  return data
-
   localStorage.setItem('stored', JSON.stringify(users));
 });
 const storedObject = JSON.parse(localStorage.getItem('stored'));
